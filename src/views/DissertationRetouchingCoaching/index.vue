0cfe05521f1ff3f1b3c818e4399eca58d51085f7
lhypoi <904579880@qq.com>
2021-05-06 23:52:01 +0800

    Update index.vue

--- src/views/DissertationRetouchingCoaching/index.vue
+++ src/views/DissertationRetouchingCoaching/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="pageSection sec1">
-      <el-image class="pageSectionImg" :src="sec1Img" fit="cover" lazy
+      <el-image class="pageSectionImg" :src="sec1Img" fit="container" lazy
         ><div slot="placeholder" class="emptyImg"
       /></el-image>
       <div class="pageSectionContent">
