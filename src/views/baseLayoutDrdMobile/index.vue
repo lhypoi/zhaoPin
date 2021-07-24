74024b652533ff985046ba91010ff66d06da71ae
lhypoi <904579880@qq.com>
2021-07-24 23:42:26 +0800

    了解更多的模块

--- src/views/baseLayoutDrdMobile/index.vue
+++ src/views/baseLayoutDrdMobile/index.vue
@@ -41,6 +41,33 @@
         <router-view :key="key" />
       </keep-alive>
     </transition>
+    <!-- 了解更多 -->
+    <div class="px-4 pt-9">
+      <div class="text-2xl text-black font-bold">
+        了解更多
+      </div>
+      <div class="text-base text-gray-500">
+        用心对待每一份订单
+      </div>
+      <div class="flex flex-row flex-wrap mt-8 mb-5">
+        <div
+          v-for="(img, imgIndex) in moreList"
+          :key="imgIndex"
+          class="w-1/3 text-center mb-5"
+        >
+          <div class="h-10 flex items-center">
+            <el-image
+              :src="img.path"
+              fit="cover"
+              class="block w-10 mb-1 mx-auto"
+            />
+          </div>
+          <div class="text-gray-500 text-sm mt-1">
+            {{ img.title }}
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- 页面底部 -->
     <div class="h-32 pt-6 pl-10 pr-10 bg-gray-200">
       <div class="text-gray-400 text-sm mb-2">
@@ -72,6 +99,44 @@ export default {
         {
           path: '/sx',
           title: '保录升学'
+        }
+      ],
+      moreList: [
+        {
+          path: require('@/assets/img_drd_mobile/more1.png'),
+          title: '专业'
+        },
+        {
+          path: require('@/assets/img_drd_mobile/more2.png'),
+          title: '高效'
+        },
+        {
+          path: require('@/assets/img_drd_mobile/more3.png'),
+          title: '1V1专属客服'
+        },
+        {
+          path: require('@/assets/img_drd_mobile/more4.png'),
+          title: '7X24'
+        },
+        {
+          path: require('@/assets/img_drd_mobile/more5.png'),
+          title: '英国G5师资'
+        },
+        {
+          path: require('@/assets/img_drd_mobile/more6.png'),
+          title: '高品质'
+        },
+        {
+          path: require('@/assets/img_drd_mobile/more7.png'),
+          title: '均分高'
+        },
+        {
+          path: require('@/assets/img_drd_mobile/more8.png'),
+          title: '提分快'
+        },
+        {
+          path: require('@/assets/img_drd_mobile/more9.png'),
+          title: 'QSTop200+'
         }
       ]
     }
