9a19405599742b454ae4f787225d60e6ecf2f134
lhypoi <904579880@qq.com>
2021-08-16 01:36:59 +0800

    feat：优化页脚组件的样式

--- src/views/baseLayoutDrdMobile/components/FoodFooter.vue
+++ src/views/baseLayoutDrdMobile/components/FoodFooter.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <div class="footer">
-      <div class=" h-16 bg-secondBgc" />
-
-      <div class=" h-64 bg-secondBgc flex justify-center align-middle flex-row flex-wrap mt-0">
-
-        <div class=" bg-secondBgc box1">
+  <div class="bg-secondBgc pt-16">
+    <!-- TODO:仅可能用必要的div实现布局，像这里的头部占位，在父级写padding-top就行 -->
+    <div class="footer container mx-auto">
+      <!-- TODO:通过flex + overflow 自适应布局，防止有些元素挤到下面去 -->
+      <div class=" h-64 bg-secondBgc flex align-middle flex-row mt-0 overflow-hidden">
+        <!-- TODO:按设计稿，左边的信息应该是占 50% -->
+        <div class=" bg-secondBgc box1 w-1/2 pr-5">
           <div class="title text-white ">展翠食品-专注糖果研发</div>
           <div class=" w-6 h-1 bg-orange mb-7 mt-5" />
           <div class="flex flex-row flex-wrap ">
@@ -21,16 +21,19 @@
                   class="block w-10 mb-1 mr-4"
                 />
               </div>
-              <div class=" text-grey text-sm h-10 leading-10 ">
+              <!-- TODO:label型的文字，确保显示完全 -->
+              <div class=" text-grey text-sm h-10 leading-10 whitespace-nowrap">
                 {{ img.title }}
               </div>
-              <div class=" text-10 h-10 leading-10 text-firstTextC font-bold">
+              <!-- TODO:内容型的文字，基于父容器剩下的宽度显示，溢出了用省略号显示，同时用html的title属性，是鼠标移上去可以显示全部文字 -->
+              <div class=" text-10 h-10 leading-10 text-firstTextC font-bold overflow-ellipsis whitespace-nowrap overflow-hidden" :title="img.info">
                 {{ img.info }}
               </div>
             </div>
           </div>
         </div>
-        <div class=" form bg-secondBgc ml-9">
+        <!-- TODO:中间的信息比较短且不容易变，所以留 1/6 -->
+        <div class=" form bg-secondBgc w-1/6">
           <div class="title text-white">联系我们</div>
           <div class=" w-6 h-1 bg-orange mt-5" />
           <div><input type="text" placeholder="姓名" class="bg-secondBgc"></div>
@@ -39,12 +42,12 @@
           <div><input type="text" placeholder="内容" class="bg-secondBgc"></div>
           <div><h1 class="">提交</h1></div>
         </div>
-
-        <div class=" bg-secondBgc flex flex-row  ml-28">
+        <!-- TODO:右边的三张图片占位比较多，所以留 1/3.并且靠右，自适应留出和中间盒子的间隙 -->
+        <div class=" bg-secondBgc flex flex-row justify-end w-1/3">
           <div
             v-for="(codeimg, imgIndex) in moreList2"
             :key="imgIndex"
-            class=" text-left mb-50 ml-12"
+            class=" text-left mb-50 ml-5 2xl:ml-12"
           >
             <div class="flex flex-row text-white">
               {{ codeimg.title }}
@@ -59,12 +62,10 @@
             </div>
           </div>
         </div>
-
       </div>
+      <!-- TODO:这里有条边界线，挪进这里，可以借用父容器的宽度 -->
+      <div class="h-10 mt-5 leading-10 bg-secondBgc text-center text-sceondTextC text-sm border-t border-gray-500 border-opacity-50">备案号：</div>
     </div>
-
-    <div class=" h-10 leading-10 bg-secondBgc text-center text-sceondTextC text-sm ">备案号：</div>
-
   </div>
 </template>
 
