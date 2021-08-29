5ad9cfbcd1411006c26a55d5a90dd69105e620a3
lhypoi <904579880@qq.com>
2021-08-29 16:15:19 +0800

    fix：tailwind问题修复

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,31 +3,28 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    // TODO：这里的调整要谨慎，细看官方文档，确保写的每一句全局规则都是在自己把握范围内的
     extend: {
-      backgroundColor:theme=>({
-        ...theme('colors'),
-        'firstBgc':'#FFF5EF',
-        'secondBgc':'#34333B',
-        'orange':'#E66717'
-      })
-    },
-    
-       textColor: theme => theme('colors'),
-       textColor: {
-         'firstTextC': '#E66717',
-         'orange1':'#FFAC05',
-         'sceondTextC': '#7D7C87',
-         'white':'#FFFFFF',
-         'grey':'#7D7C87',
-         'grey1':'#333333',
-         'grey2':'#999999',
-         'grey3':'#666666',
-         'coffee':'#452607'
-       },
-
-       borderColor: theme => ({
-        'orange': '#E66717',
-       })
+      backgroundColor: {
+        'firstBgc': '#FFF5EF',
+        'secondBgc': '#34333B',
+        'orange': '#E66717'
+      },
+      borderColor: {
+        'orange': '#E66717'
+      },
+      textColor: {
+        'firstTextC': '#E66717',
+        'orange1': '#FFAC05',
+        'sceondTextC': '#7D7C87',
+        'white': '#FFFFFF',
+        'grey': '#7D7C87',
+        'grey1': '#333333',
+        'grey2': '#999999',
+        'grey3': '#666666',
+        'coffee': '#452607'
+      }
+    }
   },
   variants: {
     extend: {
